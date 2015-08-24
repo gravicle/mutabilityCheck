4cc622d3725877106f616c3747113f90e2d68357
Amit Jain <gravicle@gmail.com>
2015-08-24 11:22:55 -0500

    Initial Commit

---
+++ ClassCheck/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ClassCheck
+//
+//  Created by Amit on 8/24/15.
+//  Copyright (c) 2015 ProductHunt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
